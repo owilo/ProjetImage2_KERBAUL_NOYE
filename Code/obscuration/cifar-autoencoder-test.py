f084ab637a1633d34b2f4948e0cf0257172d98e4
owilo <noye.valentin@gmail.com>
2024-11-17 19:08:10 +0100

    Autoencoder update

--- Code/obscuration/cifar-autoencoder-test.py
+++ Code/obscuration/cifar-autoencoder-test.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model, Model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+from PIL import Image
 
 def preprocess_image(img_path):
     img = load_img(img_path, target_size=(32, 32))
@@ -10,8 +11,14 @@ def preprocess_image(img_path):
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
-if len(sys.argv) < 4:
-    print("Format : python " + sys.argv[0] + " <Image d'entrée> <Image de sortie> <Nombre de caractéristiques obscurées (0 - 4)>")
+def array_to_pil_image(img_array):
+    img_array = np.squeeze(img_array, axis=0)
+    img_array = (img_array * 255).astype(np.uint8)
+    pil_img = Image.fromarray(img_array)
+    return pil_img
+
+if len(sys.argv) < 8:
+    print("Format : python " + sys.argv[0] + " <Image d'entrée> <Image de sortie> <x1> <y1> <x2> <y2> <Nombre de caractéristiques obscurées (0 - 4)>")
     sys.exit(1)
 
 img_path = sys.argv[1]
@@ -31,7 +38,7 @@ input_img = preprocess_image(img_path)
 
 latent = encoder.predict(input_img)
 
-n = int(sys.argv[3])
+n = int(sys.argv[7])
 
 random_indices = np.random.choice(latent.shape[1], size=n, replace=False)
 
@@ -42,5 +49,12 @@ output_img = decoder.predict(latent)
 
 output_img = np.squeeze(output_img)
 output_img = np.clip(output_img * 255, 0, 255).astype('uint8')
+
 result_img = array_to_img(output_img)
-result_img.save(sys.argv[2])
+
+crop_box = (int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6]))
+
+cropped_section = result_img.crop(crop_box)
+res = array_to_pil_image(input_img)
+res.paste(cropped_section, crop_box)
+res.save(sys.argv[2])
